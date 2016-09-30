2e8f71583dd828ca75afd564ec097e0cdd28de81
takahashisan <appler21op.mwp@docomo.ne.jp>
2016-09-30 02:08:56 +0000

    Edit html and css in index and application

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
+    @post = Post.new
   end
 
   # GET /posts/1
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   devise_for :users
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
