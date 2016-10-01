d9fca8fcdaef7bdd1f3fd10adc8fb7f54f952cb2
takahashisan <appler21op.mwp@docomo.ne.jp>
2016-10-01 05:45:11 +0000

    Added post cotroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def index
+    @users = User.page(params[:id]).per(50).order(created_at: :desc)
   end
+
+  def show
+  	 @user = User.find(params[:id])
+  end
+
+
 end
